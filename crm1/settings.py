ed494ed8e94e8a1f72256616dc6ee54e81539431
Jaydeep189 <68139837+Jaydeep189@users.noreply.github.com>
2020-09-12 12:49:41 +0530

    Update settings.py

--- crm1/settings.py
+++ crm1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'cz=&%f*9(d*zo$_55p=(p)(eki#p$pb^0159-)8k^6$9c3l&_b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://jaydeep-1.herokuapp.com/', '127.0.0.1:8000/']
+ALLOWED_HOSTS = ['jaydeep-1.herokuapp.com/', '127.0.0.1:8000/']
 
 
 # Application definition
